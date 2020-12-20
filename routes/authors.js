cfda4f046dad573f497bb422861f9ecd2642a7a7
Hadi-19 <dodo8578@gmail.com>
2020-12-20 23:20:45 +0100

    mid 5th vid commit

--- routes/authors.js
+++ routes/authors.js
@@ -34,12 +34,12 @@ router.post('/',async(req,res)=>{
 
     try{
       const newAuthor= await author.save();
-      // res.redirect(`authors/${author.id}`)
-          res.redirect(`/authors`)
+       res.redirect(`authors/${author.id}`)
+         // res.redirect(`/authors`)
         
     }
     catch{
-      res.render('authors/new.ejs', {
+      res.render('authors/edit.ejs', {
                    author,
                    errorMsg:'Error creating author'})
     }
@@ -62,13 +62,39 @@ router.post('/',async(req,res)=>{
  router.get('/:id',(req,res)=>{
    res.send('Show Author '+req.params.id);
 })
-router.get('/:id/edit',(req,res)=>{
-   console.log(req.params.id);
-   res.send('Edit Author '+req.params.id);
+router.get('/:id/edit',async(req,res)=>{
+    try{
 
+    const author=await Author.findById(req.params.id);
+    res.render('authors/edit.ejs',{author})
+    }
+    catch{
+        res.redirect('/authors')
+    }
+  
 })
-router.put('/:id',(req,res)=>{
-   res.send('Update Author '+req.params.id);
+router.put('/:id',async(req,res)=>{
+    let author
+    try{
+         author= await Author.findById(req.params.id)
+         author.name=req.body.name;
+         await author.save()
+         res.redirect(`authors/${author.id}`)
+           // res.redirect(`/authors`)
+          
+      }
+      catch{
+          if(!author){
+              res.redirect('/authors')
+          }
+          else{
+            res.render('authors/new.ejs', {
+                author,
+                errorMsg:'Error creating author'})
+
+          }
+        
+      }
 })
 router.delete('/:id',(req,res)=>{
    res.send('Delete Author '+req.params.id);
